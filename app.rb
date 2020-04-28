56c0e17bd12b6e1ef92bd621028cbea94a92dc3d
mmccartney18 <mmccartney18@punahou.edu>
2020-04-28 01:27:47 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ require './lib/hangperson_game.rb'
 
 class HangpersonApp < Sinatra::Base
 
+    require 'sinatra'
+
+class MyApp < Sinatra::Base
+  get '/' do
+    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+  end
+end
+
   enable :sessions
   register Sinatra::Flash
   
@@ -40,6 +48,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+      begin
+    if !@game.guess(letter)
+      flash[:message] = "You have already used that letter."
+    end
+    
+      rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+
     redirect '/show'
   end
   
@@ -50,11 +67,23 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    end
+    
+    if @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
+     ### YOUR CODE HERE ###
+     unless !@game.check_win_or_lose == :win
+      redirect '/show'
+      
+    end
+   
     erb :win # You may change/remove this line
   end
   
@@ -62,5 +91,6 @@ class HangpersonApp < Sinatra::Base
     ### YOUR CODE HERE ###
     erb :lose # You may change/remove this line
   end
+  
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,25 +3,71 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
-  # Get a word from remote "random word" service
+    # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word,:guesses,:wrong_guesses, :word_with_guesses, :check_win_or_lose
   
   def initialize(word)
     @word = word
-  end
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = ''
+    word.each_char do |i|
+      @word_with_guesses << '-'
+    end
+    @check_win_or_lose = :play
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
+  end
+ # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+
+  # Grab a random word from the dictionary
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  
+
+  def guess(letter)
+    #/[[:alpha]]/
+    #raise ArgumentError if guess == nil or !(letter == [[:alpha]])
+    #letter.downcase!
+    raise ArgumentError if letter.nil?
+    raise ArgumentError if letter == ''
+    raise ArgumentError if !letter.match(/[a-zA-Z]/)
+
+    
+    letter.downcase!
+      # if @word.include? letter and !@wrong_guesses? letter
+      #  return true
+    #else return false
+   # end
+    
+    if word.include? letter
+      unless guesses.include? letter
+        guesses << letter
+        for i in 0..word.length
+          if word[i] == letter
+            word_with_guesses[i] = letter
+            @check_win_or_lose = :win if !word_with_guesses.include? '-'
+          end
+        end
+        return true
+      end
+    else
+      unless wrong_guesses.include? letter
+        wrong_guesses << letter
+        if wrong_guesses.size >= 7
+          @check_win_or_lose = :lose
+        end
+        return true
+      end
+    end
+    return false
   end
 
 end
